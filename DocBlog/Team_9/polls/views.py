295ae94b385a2134c3b50eae0464661fc9762607
Mathistrg <123164665+Mathistrg@users.noreply.github.com>
2023-12-01 16:13:55 +0100

    All

--- DocBlog/Team_9/polls/views.py
+++ DocBlog/Team_9/polls/views.py
@@ -9,7 +9,7 @@ from django.shortcuts import get_object_or_404, render
 from .models import Choice, Question   
 
 def index(request):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    latest_question_list = Question.objects.order_by("-pub_date")[:]
     context = {"latest_question_list": latest_question_list}
     return render(request, "polls/index.html", context)
 
